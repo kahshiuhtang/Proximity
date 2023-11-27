14ef2a18bab71d950daec4f33e41733c78250af6
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-11-26 23:29:43 -0500

    small allowed image url fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ["uploadthing.com"] } };
+const nextConfig = { images: { domains: ["uploadthing.com", "utfs.io"] } };
 
 module.exports = nextConfig;
