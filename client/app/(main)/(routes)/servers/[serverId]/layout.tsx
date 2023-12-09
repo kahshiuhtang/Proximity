e4cd91bfc8f8a7d18d296ccb9f58b262cc8f1057
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-12-09 14:51:42 -0500

    started on server sidebar, getting server info displayed

--- client/app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ client/app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,3 +1,4 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -27,6 +28,13 @@ const ServerIdLayout = async ({
   if (!server) {
     return redirect("/");
   }
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
 };
 export default ServerIdLayout;
