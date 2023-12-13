3e09ddc2be55a7462a94878bc7d1bbe8a9fe4437
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-12-13 18:46:59 -0500

    fix to not working invite code modal

--- client/app/api/servers/[serverId]/invite-code/route.ts
+++ client/app/api/servers/[serverId]/invite-code/route.ts
@@ -1,20 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
+import { NextResponse } from "next/server";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } }
 ) {
   try {
     const profile = await currentProfile();
+
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!params.serverId) {
-      return new NextResponse("Server Id Missing", { status: 400 });
+      return new NextResponse("Server ID Missing", { status: 400 });
     }
+
     const server = await db.server.update({
       where: {
         id: params.serverId,
@@ -24,9 +28,10 @@ export async function PATCH(
         inviteCode: uuidv4(),
       },
     });
+
     return NextResponse.json(server);
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log("[SERVER_ID]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
--- client/components/modals/invite-modal.tsx
+++ client/components/modals/invite-modal.tsx
@@ -1,21 +1,33 @@
 "use client";
+
+import axios from "axios";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useState } from "react";
+
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { useState } from "react";
-import { useModal } from "@/hooks/use-model-store";
 import { Label } from "@/components/ui/label";
+import { useModal } from "@/hooks/use-model-store";
 import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
-import { Check, Copy, RefreshCw } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { useOrigin } from "@/hooks/use-origin";
-import axios from "axios";
 
 export const InviteModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const origin = useOrigin();
+
+  const isModalOpen = isOpen && type === "invite";
+  const { server } = data;
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
   const onCopy = () => {
     navigator.clipboard.writeText(inviteUrl);
     setCopied(true);
@@ -25,24 +37,21 @@ export const InviteModal = () => {
     }, 1000);
   };
 
-  const { onOpen, isOpen, onClose, type, data } = useModal();
   const onNew = async () => {
     try {
       setIsLoading(true);
       const response = await axios.patch(
         `/api/servers/${server?.id}/invite-code`
       );
+
       onOpen("invite", { server: response.data });
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
-  const origin = useOrigin();
-  const { server } = data;
-  const [copied, setCopied] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const isModalOpen = isOpen && type === "invite";
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -53,14 +62,14 @@ export const InviteModal = () => {
         </DialogHeader>
         <div className="p-6">
           <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-            Server Invite Link
+            Server invite link
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
               disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
               value={inviteUrl}
-            ></Input>
+            />
             <Button disabled={isLoading} onClick={onCopy} size="icon">
               {copied ? (
                 <Check className="w-4 h-4" />
@@ -76,9 +85,9 @@ export const InviteModal = () => {
             size="sm"
             className="text-xs text-zinc-500 mt-4"
           >
-            Generate New Link
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
           </Button>
-          <RefreshCw className="w-4 h-4 ml-2" />
         </div>
       </DialogContent>
     </Dialog>
