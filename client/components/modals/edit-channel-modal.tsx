4eb30312a0b92c57282471ccbfa88ed333170d70
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-12-23 10:41:44 -0500

    finished back end route for patching channels

--- client/components/modals/edit-channel-modal.tsx
+++ client/components/modals/edit-channel-modal.tsx
@@ -72,7 +72,7 @@ export const EditChannelModal = () => {
           serverId: server?.id,
         },
       });
-      await axios.post(url, values);
+      await axios.patch(url, values);
       form.reset();
       router.refresh();
       onClose();
