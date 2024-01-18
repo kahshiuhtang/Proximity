9a48f69f2d24a46bec70a475635e6bc2cf85f2d5
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-17 22:57:44 -0500

    finished up chat input

--- client/components/chat/chat-input.tsx
+++ client/components/chat/chat-input.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import axios from "axios";
+import qs from "query-string";
+
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -7,7 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Plus, Smile } from "lucide-react";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -24,8 +27,14 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async (value: z.infer<typeof formSchema>) => {
-    console.log(value);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({ url: apiUrl, query });
+
+      await axios.post(url, values);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <Form {...form}>
@@ -40,10 +49,21 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                   <button
                     type="button"
                     onClick={() => {}}
-                    className="absolute topo-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]"></Plus>
                   </button>
+                  <Input
+                    disabled={isLoading}
+                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                    {...field}
+                  />
+                  <div className="absolute top-7 right-8">
+                    <Smile />
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
