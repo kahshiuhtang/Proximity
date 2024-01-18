702f23d545d54ec18c7284ab03e4085745beae74
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-18 06:57:12 -0500

    work on adding a pdf file

--- client/components/chat/chat-input.tsx
+++ client/components/chat/chat-input.tsx
@@ -11,6 +11,7 @@ import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
 import { Plus, Smile } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -20,6 +21,8 @@ interface ChatInputProps {
 }
 const formSchema = z.object({ content: z.string().min(1) });
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: { content: "" },
@@ -48,7 +51,9 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => {
+                      onOpen("messageFile", { apiUrl, query });
+                    }}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]"></Plus>
