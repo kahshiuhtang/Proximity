8467c01e743bb4f8f558f1b60f33b405a1a8f055
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-18 16:00:48 -0500

    started work on handling errors with chat messages

--- client/components/chat/chat-welcome.tsx
+++ client/components/chat/chat-welcome.tsx
@@ -16,7 +16,11 @@ export const ChatWelcome = ({ name, type }: ChatWelcomeProps) => {
         {type === "channel" ? "Welcome to #" : ""}
         {name}
       </p>
-      <p className="text-zinc-600 dark:text-zinc-400 text"></p>
+      <p className="text-zinc-600 dark:text-zinc-400 text-sm">
+        {type === "channel"
+          ? `This is the start of the #${name} channel.`
+          : `This is the start of your conversation with ${name}`}
+      </p>
     </div>
   );
 };
