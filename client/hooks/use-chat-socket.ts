bf46714050d0b436664544de90c361cabb69890d
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-20 17:56:39 -0500

    work on use-chat-socket hook

--- client/hooks/use-chat-socket.ts
+++ client/hooks/use-chat-socket.ts
@@ -31,7 +31,48 @@ export const useCHatSocket = ({
         if (!oldData || !oldData.pages || oldData.pages.length === 0) {
           return onloadeddata;
         }
+
+        const newData = oldData.pages.map((page: any) => {
+          return {
+            ...page,
+            items: page.items.map((item: MessageWithMemberWithProfile) => {
+              if (item.id === message.id) {
+                return message;
+              }
+              return item;
+            }),
+          };
+        });
+        return {
+          ...oldData,
+          pages: newData,
+        };
       });
     });
-  }, []);
+    socket.on(addKey, (message: MessageWithMemberWithProfile) => {
+      queryClient.setQueryData([queryKey], (oldData: any) => {
+        if (!oldData || !oldData.pages || oldData.pages.length === 0) {
+          return {
+            pages: [{ items: [message] }],
+          };
+        }
+
+        const newData = [...oldData.pages];
+
+        newData[0] = {
+          ...newData[0],
+          items: [message, ...newData[0].items],
+        };
+        return {
+          ...oldData,
+          pages: newData,
+        };
+      });
+    });
+
+    return () => {
+      socket.off(addKey);
+      socket.off(updateKey);
+    };
+  }, [queryClient, addKey, queryKey, socket, updateKey]);
 };
