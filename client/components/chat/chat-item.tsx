ed98764ce0b9bccdca4672addab850dc10283ad8
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-20 11:29:37 -0500

    started chat item

---
+++ client/components/chat/chat-item.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Member, Profile } from "@prisma/client";
+
+interface ChatItemProps {
+  id: string;
+  content: string;
+  member: Member & {
+    profile: Profile;
+  };
+  timestamp: string;
+  fileUrl: string | null;
+  deleted: boolean;
+  currentMember: Member;
+  isUpdated: boolean;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+}
+export const ChatItem = ({
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  currentMember,
+  isUpdated,
+  socketUrl,
+  socketQuery,
+}: ChatItemProps) => {
+  return (
+    <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full"></div>
+  );
+};
