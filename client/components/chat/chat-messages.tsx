d2b970dc7e0dcd089f159cd4a761247017d4a432
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-20 19:01:07 -0500

    allowing for autoscroll

--- client/components/chat/chat-messages.tsx
+++ client/components/chat/chat-messages.tsx
@@ -9,6 +9,7 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { ChatWelcome } from "./chat-welcome";
 import { ChatItem } from "./chat-item";
 import { useChatSocket } from "@/hooks/use-chat-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MM yyyy, HH:mm";
 
@@ -51,9 +52,17 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
-  useChatSocket({ queryKey, addKey, updateKey });
   const chatRef = useRef<ElementRef<"div">>(null);
   const bottomRef = useRef<ElementRef<"div">>(null);
+  useChatSocket({ queryKey, addKey, updateKey });
+  useChatScroll({
+    chatRef,
+    bottomRef,
+    loadMore: fetchNextPage,
+    shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+    count: data?.pages?.[0]?.items?.length ?? 0,
+  });
+
   if (status === "pending") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
